bb064dc67241e7da099040cff277b708db356d82
Stargazer3240 <112516550+fabriciomj@users.noreply.github.com>
2025-09-16 18:45:07 -0300

    Adiciona o model Cardapio

--- cardapio/models.py
+++ cardapio/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class Prato(models.Model):
+    nome = models.CharField(max_length=30)
+    ingredientes = models.CharField(max_length=250)
+    foto_cardapio = models.ImageField()
+    foto_carrossel = models.ImageField(blank=True)
+    preco = models.DecimalField(max_digits=7, decimal_places=2)
