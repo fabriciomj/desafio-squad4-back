8b978db14bd636f213a5440a4e358be59d4e94f1
Fabricio Jacome <112516550+fabriciomj@users.noreply.github.com>
2025-09-20 20:02:09 -0300

    Adiciona o app cozinha

--- squad4/settings.py
+++ squad4/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ["fabriciomj.pythonanywhere.com", "127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
+    "cozinha",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
