26e0c7e82513bdbc310c5a71af631ff8e3b20b57
ArturDaniel123 <artur.daniel.borges2@hotmail.com>
2025-09-21 17:01:52 -0300

    criada a função index no arquivo views.py e a rota do index no urls.py

--- squad4/urls.py
+++ squad4/urls.py
@@ -20,8 +20,11 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 
+from cozinha.views import index
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', index)
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
