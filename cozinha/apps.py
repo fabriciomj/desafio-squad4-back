6ce5f9e8b33ffa6f4f7a987ec999665e93172557
Fabricio Jacome <112516550+fabriciomj@users.noreply.github.com>
2025-09-20 22:54:31 -0300

    Formata o arquivo usando ruff

--- cozinha/apps.py
+++ cozinha/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class CozinhaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'cozinha'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "cozinha"
