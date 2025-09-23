7904cf97f6badc08fed16d2029b809805e79ccc6
Fabricio Jacome <112516550+fabriciomj@users.noreply.github.com>
2025-09-23 19:46:09 -0300

    Modifica a maneira que o contexto do cardápio é gerado
    Dessa forma, fica mais simples adicionar novas categorias ao cardápio.

--- cozinha/views.py
+++ cozinha/views.py
@@ -19,15 +19,21 @@ def index(request):
 
 
 def cardapio(request):
-    entradas = Prato.objects.filter(tipo__exact="E")
-    principais = Prato.objects.filter(tipo__exact="PP")
-    sobremesas = Prato.objects.filter(tipo__exact="S")
-    pratos = {
-        "entradas": entradas,
-        "principais": principais,
-        "sobremesas": sobremesas,
-    }
-    return render(request, "cardapio.html", pratos)
+    # Declara a tupla de tipos, em vez de usar TIPOS_PRATOS.keys() para garantir a ordem.
+    tipos = ("E", "M", "V", "S", "B")
+    categorias = [Prato.TIPOS_PRATOS[tipo] for tipo in tipos]
+
+    # Pratos é uma lista de listas, na qual as sublistas são os pratos de um determinado tipo.
+    pratos = []
+    for tipo in tipos:
+        pratos_cat_atual = []
+        for prato in Prato.objects.filter(tipo__exact=tipo):
+            pratos_cat_atual.append(prato)
+        pratos.append(pratos_cat_atual)
+
+    # Usa a função zip para ter fácil acesso aos nomes das categorias no template.
+    cats_pratos = zip(categorias, pratos, strict=True)
+    return render(request, "cardapio.html", {"cats_pratos": cats_pratos})
 
 
 def sobre_nos(request):
